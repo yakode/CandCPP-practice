a89b72064a68afd9ae56cefc256beab52d2c3004
yk <re703729@gmail.com>
2023-05-17 15:30:55 +0800

    LeetCode/2130 Add A Solution

--- LeetCode/2130_Maximum-Twin-Sum-of-a-Linked-List.cpp
+++ LeetCode/2130_Maximum-Twin-Sum-of-a-Linked-List.cpp
@@ -1,3 +1,43 @@
+// two pointer!
+class Solution{
+public:
+    int pairSum(ListNode* head){
+        int maxSum = 0, twinSum;
+        ListNode *half1, *half2;
+        ListNode *prev, *ptr; 
+
+        // find the head of the second half (head2)
+        // half1 is a temperate pointer to find half2
+        half1 = head->next;
+        half2 = head;
+        while(half1->next){
+            half1 = half1->next->next;
+            half2 = half2->next;
+        }
+        half1 = head;
+        half2 = half2->next;
+        
+        // reverse the second half of linked list
+        prev = half2;
+        while(prev->next){
+            ptr = prev->next;
+            prev->next = prev->next->next;
+            ptr->next = half2;
+            half2 = ptr;
+        }
+
+        // calculate each twin sum
+        while(half2){
+            twinSum = half1->val + half2->val;
+            if(twinSum > maxSum) maxSum = twinSum;
+            half1 = half1->next;
+            half2 = half2->next;
+        }
+
+        return maxSum;
+    }
+};
+
 // stack
 class Solution{
 public:
@@ -39,7 +79,6 @@ public:
         int n = 0;
         int maxSum = 0, twinSum;
         ListNode *ptr = head, *head2, *cur;
-        stack<int> s;
 
         // count the size of linked-list
         while(ptr != nullptr){
