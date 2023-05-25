869ab7937f2136c1a6e413221c6edc9452a4e002
yk <re703729@gmail.com>
2023-05-25 18:22:31 +0800

    LeetCode/0167 Add A Solution

--- LeetCode/0167_Two-Sum-II-Input-Array-Is-Sorted.cpp
+++ LeetCode/0167_Two-Sum-II-Input-Array-Is-Sorted.cpp
@@ -1,6 +1,25 @@
-class Solution {
+// two pointers
+class Solution{
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
+    vector<int> twoSum(vector<int> &numbers, int target){
+        int left = 0, right = numbers.size() - 1;
+
+        while(left < right){
+            int sum = numbers[left] + numbers[right];
+            
+            if(sum < target) left++;
+            else if(sum > target) right--;
+            else return vector<int> {left+1, right+1};
+        }
+
+        return vector<int> {-1, -1};
+    }
+};
+
+// binary search
+class Solution{
+public:
+    vector<int> twoSum(vector<int> &numbers, int target){
         for(int i = 0; i < numbers.size(); ++i){
             int left = i+1, right = numbers.size(), mid;
             int target2 = target - numbers[i];
