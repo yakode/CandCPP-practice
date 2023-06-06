b5ce19f851bf6b64eff6fa2aa9f197e1da46a541
yk <re703729@gmail.com>
2023-06-06 15:22:34 +0800

    LeetCode/0015 Add A Solution

--- LeetCode/0015_3Sum.cpp
+++ LeetCode/0015_3Sum.cpp
@@ -1,6 +1,42 @@
-class Solution {
+// two pointers
+class Solution{
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> threeSum(vector<int> &nums){
+        vector<vector<int>> ans;
+        
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < nums.size(); ++i){
+            if(i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+
+            int j = i + 1, k = nums.size() - 1;
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if(sum < 0){
+                    j++;
+                }else if(sum > 0){
+                    k--;
+                }else{
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    while(j < k && nums[j-1] == nums[j]) j++;
+                    k--;
+                    while(j < k && nums[k+1] == nums[k]) k--;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+
+// binary search
+class Solution{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums){
         vector<vector<int>> ans;
         int N = nums.size();
         sort(nums.begin(), nums.end());
