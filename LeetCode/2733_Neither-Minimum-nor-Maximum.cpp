219510051b1415cfd24f8167e5d45d16df077fc2
yakode <y8976c@gmail.com>
2023-06-11 14:09:59 +0800

    LeetCode/2733 Add A Solution

--- LeetCode/2733_Neither-Minimum-nor-Maximum.cpp
+++ LeetCode/2733_Neither-Minimum-nor-Maximum.cpp
@@ -16,3 +16,14 @@ public:
         
     }
 };
+
+class Solution{
+public:
+    int findNonMinOrMax(vector<int> &nums){
+        if(nums.size() < 3) return -1;
+
+        sort(nums.begin(), nums.begin() + 3);
+
+        return nums[1];
+    }
+};
