81881f63ee82f44175dbae044795a6b734497955
yakode <y8976c@gmail.com>
2023-06-24 00:30:34 +0800

    LeetCode/0206 on 2023/06/24

---
+++ LeetCode/0206_Reverse-Linked-List.cpp
@@ -0,0 +1,15 @@
+class Solution{
+public:
+    ListNode* reverseList(ListNode *head){
+        ListNode *prev = nullptr, *cur = head, *next;
+
+        while(cur){
+            next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+
+        return prev;
+    }
+};
