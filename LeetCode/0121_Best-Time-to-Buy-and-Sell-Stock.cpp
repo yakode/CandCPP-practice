d056f73a63a19516d8ce91f11ff8e7da98b518b5
yakode <y8976c@gmail.com>
2023-07-21 00:04:46 +0800

    LeetCode/0121 on 2023/07/19

---
+++ LeetCode/0121_Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:
+    int maxProfit(vector<int> &prices){
+        int ans = 0;
+        int mini = prices[0];
+
+        for(int i = 1; i < prices.size(); ++i){
+            mini = min(mini, prices[i]);
+            ans = max(ans, prices[i] - mini);
+        }
+
+        return ans;
+    }
+};
