dae8e9c7dde91a7d5b5a25a8fa9a2887d850163b
yakode <y8976c@gmail.com>
2023-07-22 00:59:28 +0800

    LeetCode/0058 on 2023/07/22

---
+++ LeetCode/0058_Length-of-Last-Word.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:
+    int lengthOfLastWord(string s){
+        int ans = 0;
+        int i = s.size() - 1;
+        
+        while(s[i] == ' ')
+            --i;
+        while(i >= 0 && s[i--] != ' ')
+            ++ans;
+
+        return ans;
+    }
+};
