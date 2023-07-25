72661fb3673df0dfbac10c1efc1e172bc90bb15b
yakode <y8976c@gmail.com>
2023-07-25 21:09:49 +0800

    LeetCode/0077 on 2023/07/25

---
+++ LeetCode/0077_Combinations.cpp
@@ -0,0 +1,24 @@
+class Solution{
+    void dfs(int i, int n, int k, vector<int> &tmp, vector<vector<int>> &ans){
+        if(k == 0){
+            ans.push_back(tmp);
+            return;
+        }
+        if(i > n)
+            return;
+
+        dfs(i + 1, n, k, tmp, ans);
+        tmp[tmp.size() - k] = i;
+        dfs(i + 1, n, k - 1, tmp, ans);   
+    }
+
+public:
+    vector<vector<int>> combine(int n, int k){
+        vector<vector<int>> ans;
+        vector<int> tmp(k);
+
+        dfs(1, n, k, tmp, ans);
+
+        return ans;
+    }
+};
